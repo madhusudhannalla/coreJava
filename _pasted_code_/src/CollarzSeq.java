2c98f3085b930744922b0b430d1f3a6985410463
madhusudhannalla <madhusudhan.nalla@ojas-it.com>
2022-01-26 15:26:23 +0530

    hai pushed

--- _pasted_code_/src/CollarzSeq.java
+++ _pasted_code_/src/CollarzSeq.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class CollarzSeq {
 String name="madhu";
-
+String hai="hai";
 
 	public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
